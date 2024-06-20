3a41de92ec2058431b5068512e1dfc8935a14860
wangenze <15041567431@126.com>
2024-06-20 11:47:15 +0800

    :tada: 项目初始化

---
+++ src/main.js
@@ -0,0 +1,21 @@
+import './assets/main.css'
+import DevUI from 'vue-devui';
+import 'vue-devui/style.css';
+import '@devui-design/icons/icomoon/devui-icon.css';
+import { ThemeServiceInit, infinityTheme } from 'devui-theme';
+
+ThemeServiceInit({ infinityTheme }, 'infinityTheme');
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+app.use(DevUI)
+
+app.mount('#app')
